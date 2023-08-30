83647fbebb7c14b0c663de05d8b6b662b5eb40a8
Sairaj Chouhan <sairaj2119@gmail.com>
2023-08-30 18:44:46 +0530

    change header and footer to gql

--- helper/index.ts
+++ helper/index.ts
@@ -1,8 +1,9 @@
+import * as Utils from "@contentstack/utils";
 import { addEditableTags } from "@contentstack/utils";
 import { Page, BlogPosts } from "../typescript/pages";
 import getConfig from "next/config";
 import { FooterProps, HeaderProps } from "../typescript/layout";
-import { getEntry, getEntryByUrl } from "../contentstack-sdk";
+import { getEntry, getEntryByUrl, renderOption } from "../contentstack-sdk";
 
 const { publicRuntimeConfig } = getConfig();
 const envConfig = process.env.CONTENTSTACK_API_KEY
@@ -10,26 +11,218 @@ const envConfig = process.env.CONTENTSTACK_API_KEY
   : publicRuntimeConfig;
 
 const liveEdit = envConfig.CONTENTSTACK_LIVE_EDIT_TAGS === "true";
+import ContentstackLivePreview from "@contentstack/live-preview-utils";
+
+const graphqlUrl = new URL(
+  `https://dev11-graphql.csnonprod.com/stacks/${process.env.CONTENTSTACK_API_KEY}?environment=${process.env.CONTENTSTACK_ENVIRONMENT}`
+);
+
+const GRAPHQL_HOST_NAME = "dev11-graphql.csnonprod.com";
+const LIVE_PREVIEW_HOST_NAME = "dev11-preview.csnonprod.com";
+
+function getHeaders() {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append(
+    "access_token",
+    process.env.CONTENTSTACK_DELIVERY_TOKEN as string
+  );
+  return headers;
+}
+
+const gqlRequest = async (
+  gql: string,
+  options?: {
+    variables?: Record<string, any>;
+    operationName?: string;
+  }
+) => {
+  const hash = ContentstackLivePreview.hash;
+  const headers = getHeaders();
+
+  if (hash) {
+    headers.append("live_preview", hash);
+    headers.append(
+      "authorization",
+      process.env.CONTENTSTACK_MANAGEMENT_TOKEN as string
+    );
+    graphqlUrl.hostname = LIVE_PREVIEW_HOST_NAME;
+  } else {
+    graphqlUrl.hostname = GRAPHQL_HOST_NAME;
+  }
+
+  const body: Record<string, any> = {
+    query: gql,
+  };
+
+  body.variables = options?.variables || null;
+  if (options?.operationName) {
+    body.operationName = options.operationName;
+  }
+
+  const res = await fetch(graphqlUrl.toString(), {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(body),
+  });
+
+  return res;
+};
 
 export const getHeaderRes = async (): Promise<HeaderProps> => {
-  const response = (await getEntry({
-    contentTypeUid: "header",
-    referenceFieldPath: ["navigation_menu.page_reference"],
-    jsonRtePath: ["notification_bar.announcement_text"],
-  })) as HeaderProps[][];
+  const gql = `
+query HeaderQuery {
+  all_header {
+    total
+    items {
+      title
+      logoConnection {
+        edges {
+          node {
+            url
+            filename
+          }
+        }
+      }
+      navigation_menu {
+        label
+        page_referenceConnection {
+          totalCount
+          edges {
+            node {
+              ... on Page {
+                title
+                url
+              }
+            }
+          }
+        }
+      }
+      notification_bar {
+        show_announcement
+        announcement_text {
+          json
+        }
+      }
+      system {
+        uid
+      }
+    }
+  }
+}
+`;
 
-  liveEdit && addEditableTags(response[0][0], "header", true);
-  return response[0][0];
+  const res = await gqlRequest(gql);
+
+  const data = await res.json();
+
+  console.log("dddddddddddddddddddddddddd", data);
+  const header = data.data.all_header.items[0];
+
+  const transformed = {
+    ...header,
+    logo: header.logoConnection.edges[0].node,
+    navigation_menu: header.navigation_menu.map((item: any) => ({
+      ...item,
+      page_reference: item.page_referenceConnection.edges.map(
+        (edge: any) => edge.node
+      ),
+    })),
+    uid: header.system.uid,
+    notification_bar: {
+      ...header.notification_bar,
+    },
+  };
+
+  Utils.GQL.jsonToHTML({
+    entry: transformed,
+    paths: ["notification_bar.announcement_text"],
+    renderOption: renderOption,
+  });
+
+  liveEdit && Utils.addEditableTags(transformed, "header", true);
+
+  return transformed;
 };
 
 export const getFooterRes = async (): Promise<FooterProps> => {
-  const response = (await getEntry({
-    contentTypeUid: "footer",
-    referenceFieldPath: undefined,
-    jsonRtePath: ["copyright"],
-  })) as FooterProps[][];
-  liveEdit && addEditableTags(response[0][0], "footer", true);
-  return response[0][0];
+  const gql = `
+query FooterQuery {
+  all_footer {
+    total
+    items {
+      title
+      logoConnection {
+        edges {
+          node {
+            url
+            filename
+          }
+        }
+      }
+      navigation {
+        link {
+          href
+          title
+        }
+      }
+      social {
+        social_share {
+          link {
+            href
+            title
+          }
+          iconConnection {
+            edges {
+              node {
+                url
+                filename
+              }
+            }
+          }
+        }
+      }
+      copyright {
+        json
+      }
+      system {
+        uid
+      }
+    }
+  }
+}
+
+`;
+
+  const res = await gqlRequest(gql);
+
+  const data = await res.json();
+
+  const footer = data.data.all_footer.items[0];
+
+  const transformed = {
+    ...footer,
+    logo: footer.logoConnection.edges[0].node,
+    social: {
+      social_share: footer.social.social_share.map((item: any) => ({
+        ...item,
+        icon: item.iconConnection.edges[0].node,
+      })),
+    },
+    uid: footer.system.uid,
+    copyright: {
+      ...footer.copyright,
+    },
+  };
+
+  Utils.GQL.jsonToHTML({
+    entry: transformed,
+    paths: ["copyright"],
+    renderOption: renderOption,
+  });
+
+  liveEdit && Utils.addEditableTags(transformed, "footer", true);
+  return transformed as FooterProps;
 };
 
 export const getPageRes = async (entryUrl: string): Promise<Page> => {
